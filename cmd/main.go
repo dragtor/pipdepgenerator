88ebb56f2aa1c95ace3a63ccc6867d23419437a9
dragtor <shubhamshahare169@gmail.com>
2020-08-30 00:39:18 +0530

    Added method to generate requirement.txt

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,24 @@
-package main
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/dragtor/pipdepgenerator/pkg/fsutils"
+	"github.com/dragtor/pipdepgenerator/pkg/parse"
+)
+
+func GenerateRequirementTxt(root string, reqtxtpath string) {
+	packages := make(map[string]bool)
+	filePaths, _ := fsutils.FilePathWalkDir(root, []string{"py"}, []string{"."})
+	for _, path := range filePaths {
+		contents := parse.ReadFileLineByLine(path)
+		importStmtList := parse.FilterImportStatements(contents)
+		for _, stmt := range importStmtList {
+			pkgs := parse.TokenizeImportedPackage(stmt)
+			for _, pkg := range pkgs {
+				packages[pkg] = true
+			}
+		}
+	}
+	fmt.Println(packages)
+}
---
+++ cmd/main_test.go
@@ -0,0 +1,7 @@
+package cmd
+
+import "testing"
+
+func TestGenerateRequirementTxt(t *testing.T) {
+	GenerateRequirementTxt("/Users/shubham/agrostar/ticketservice/", "")
+}
