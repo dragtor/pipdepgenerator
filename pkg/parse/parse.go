57c29f8c6505d8cb9b8d62c513d95c7d7b45d66e
dragtor <shubhamshahare169@gmail.com>
2020-08-29 20:42:24 +0530

    tokenize import statement and return list of packages

--- pkg/parse/parse.go
+++ pkg/parse/parse.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"os"
 	"regexp"
+	"strings"
 )
 
 func ReadFile(path string) string {
@@ -34,14 +35,60 @@ func ReadFileLineByLine(path string) []string {
 	return data
 }
 
-func GetImportedLibrariesList(lines []string) []string {
+func FilterImportStatements(lines []string) []string {
 	var libstmtList []string
 	for _, line := range lines {
-		regExpr := "([ ]*from[ ]+[a-zA-Z0-9]+[.a-zA-Z_0-9]*)*[ ]*import[ ]+([a-zA-Z_0-9]+[.[a-zA-Z_0-9]*)[ ]*([ ]*,[ ]*[a-zA-Z_0-9]+)*[ \n]*$"
+		regExpr := `([\s]*^from[\s]+([_\w]+)(.[_\w]+)*[\s]+)?import[\s]+[\w]+(.[\w]+)*[\s]*(as[\s][\w]+)?(,[\s]*[\w]+[\s]*(as[\s][\w]+)?)*$`
 		validImportStmt := regexp.MustCompile(regExpr)
 		if validImportStmt.MatchString(line) {
 			libstmtList = append(libstmtList, line)
 		}
 	}
 	return libstmtList
+}
+
+type Package string
+type ImportStatmentAST struct {
+	PackageList []Package
+}
+
+func ListPackagesInImportSyntax(classStmt string) []string {
+	tokens := strings.Split(classStmt, ",")
+	var pkgList []string
+	for _, pkg := range tokens {
+		p := strings.TrimSpace(pkg)
+		pkgList = append(pkgList, p)
+	}
+	return pkgList
+}
+
+func ListPackagesInFromSytax(fromStmt string) []string {
+	baseTokenList := strings.Split(fromStmt, ",")
+	var pkgList []string
+	for _, baseToken := range baseTokenList {
+		basepkgnamewithsubmodule := strings.TrimSpace(baseToken)
+		submoduleList := strings.Split(basepkgnamewithsubmodule, ".")
+		pkgList = append(pkgList, submoduleList[0])
+	}
+	return pkgList
+}
+
+func TokenizeImportedPackage(importStatement string) []string {
+	regExpr := `(from (?P<package>[, .\w]+))*import (?P<classes>[,. \w]+)`
+	pathMetadata := regexp.MustCompile(regExpr)
+	matches := pathMetadata.FindStringSubmatch(importStatement)
+	names := pathMetadata.SubexpNames()
+	var tmpMap = map[string]string{}
+	for i, match := range matches {
+		if i != 0 {
+			tmpMap[names[i]] = match
+		}
+	}
+	if packageList, ok := tmpMap["package"]; ok {
+		if strings.TrimSpace(packageList) == "" {
+			return ListPackagesInImportSyntax(tmpMap["classes"])
+		}
+		return ListPackagesInFromSytax(tmpMap["package"])
+	}
+	return []string{}
 }
--- pkg/parse/parse_test.go
+++ pkg/parse/parse_test.go
@@ -45,16 +45,49 @@ type TestCase3 struct {
 
 var TestCases3 = []TestCase3{
 	TestCase3{
-		InputStr:       []string{"from some.Resource import something,somethinganother ,ok"},
+		InputStr:       []string{"from some.Resource import something,somethinganother ,ok", "for i in range:\n"},
 		ExpectedOutput: []string{"from some.Resource import something,somethinganother ,ok"},
 	},
 }
 
-func TestGetImportedLibrariesList(t *testing.T) {
+func TestFilterImportStatements(t *testing.T) {
 	for _, tc := range TestCases3 {
-		actualResult := GetImportedLibrariesList(tc.InputStr)
+		actualResult := FilterImportStatements(tc.InputStr)
 		if !reflect.DeepEqual(tc.ExpectedOutput, actualResult) {
 			t.Errorf("Failed")
+		}
+	}
+}
+
+type TestCase4 struct {
+	Importstatement string
+	ExpectedPkgName []string
+}
+
+var TC4 = []TestCase4{
+	TestCase4{
+		Importstatement: "from dotenv import load_dotenv",
+		ExpectedPkgName: []string{"dotenv"},
+	},
+	TestCase4{
+		Importstatement: "from os.path import dirname, abspath",
+		ExpectedPkgName: []string{"os"},
+	},
+	TestCase4{
+		Importstatement: "from django.contrib.auth.models import Group",
+		ExpectedPkgName: []string{"django"},
+	},
+	TestCase4{
+		Importstatement: "import os,random",
+		ExpectedPkgName: []string{"os", "random"},
+	},
+}
+
+func TestTokenizeImportedPackage(t *testing.T) {
+	for _, tc := range TC4 {
+		actualResult := TokenizeImportedPackage(tc.Importstatement)
+		if !reflect.DeepEqual(actualResult, tc.ExpectedPkgName) {
+			t.Errorf(fmt.Sprintf("Failed expected %+v actual %+v", tc.ExpectedPkgName, actualResult))
 		}
 	}
 }
